fa3e577b04daf3f94f7865491c3e85622d91ec03
Gabriela <146922132+Gabi3fnv@users.noreply.github.com>
2023-10-08 21:32:21 +0200

    Add files via upload

---
+++ exboucle.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+let a=prompt("Choisissez un chiffre entre 1 et 10");
+for(let i=0;i<a;i++){
+    console.log('OKLM');
+}   
+
+//correction//
+
+let aBis=4;
+for(let i=0;i<aBis;i++){
+    console.log('OKLM');
+}
+
